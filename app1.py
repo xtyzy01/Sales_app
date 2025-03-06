057ada1a254994b2315cb6f8d73fbef51dafe813
Esther Okorie <153503683+xtyzy01@users.noreply.github.com>
2025-03-06 18:01:10 +0100

    app1.py

--- app1.py
+++ app1.py
@@ -4,7 +4,7 @@ import sklearn
 
 #load the model
 
-with open('AI specialisation/Adsales_model.pkl', 'rb') as file:
+with open('Adsales_model.pkl', 'rb') as file:
     model = pickle.load(file)
 
 import streamlit as st
@@ -12,12 +12,12 @@ import streamlit as st
 
 #streamlit UI
 st.title('Sales Prediction App')
-st.write('This app predicts the **Sales** amount!')
+st.write('This app estimates the potential revenue a business can generate based on its investment in radio, TV, and social media advertising.')
 st.write('Please input the following parameters:')
 
 #Input form
-Radio_Amount=st.number_input('Radio',min_value=1000.0, max_value=999999.0, value=2000.0)
 Tv_Amount=st.number_input('TV',min_value=1000.0, max_value=999999.0, value=2000.0)
+Radio_Amount=st.number_input('Radio',min_value=1000.0, max_value=999999.0, value=2000.0)
 Socialmedia_Amount=st.number_input('Socialmedia',min_value=1000.0, max_value=999999.0, value=2000.0)
 
 
